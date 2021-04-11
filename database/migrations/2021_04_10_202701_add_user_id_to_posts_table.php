62b4b64baa3efa072935385df2327ccdc68d20bc
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-04-11 20:21:05 +0900

    Add some columns for posts

---
+++ database/migrations/2021_04_10_202701_add_user_id_to_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->OnDelete('cascade');
+            $table->string('part');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+            $table->dropColumn('part');
+            // $table->dropForeign('posts_user_id_foreign')
+        });
+    }
+}
