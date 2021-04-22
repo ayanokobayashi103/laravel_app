f79c810f3164402b8fabb16f8d9196d7c13e19e9
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-04-22 12:48:17 +0900

    Add background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="{{ asset('css/post.css') }}" rel="stylesheet">
 
 </head>
 
